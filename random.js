32cfafbe75aa5d18ad6075ac57f32c5e39b768a1
sulphite <49396588+sulphite@users.noreply.github.com>
2020-09-08 13:07:11 +0200

    added total cost calculation

--- random.js
+++ random.js
@@ -1,24 +1,34 @@
-const horns = ["small","med","large","stubby","slight curve","ram","angled","curved","lightning","gravity"];
-const ears = ["farm","triangle","long","fan","flop","low"];
-const cheek = ["tuft","bare","fin","pad","gills","fluff"];
-const tail = ["lion","tuft","curl","lizard","horse","brush","bun","fin","paddle","shark"];
-let beaft = {}
+const [a,b,c] = [1,2,3];
+const data = {
+    horns: [["small",a],["med",a],["large",a],["stubby",a],["slight curve",a],["ram",b],["angled",b],["curved",b],["lightning",c],["gravity",c]],
+    ears: [["farm",a],["triangle",a],["long",a],["fan",b],["flop",b],["low",c]],
+    cheek: [["tuft",a],["bare",a],["fin",b],["pad",b],["gills",c],["fluff",b]],
+    tail: [["lion",a],["tuft",a],["curl",b],["lizard",a],["horse",b],["brush",b],["bun",b],["fin",c],["paddle",c],["shark",c]]
+}
+
+let beaft = {};
+let total = 0;
 
 function getRandom(arr) {
-    return arr[Math.floor(Math.random() * arr.length)]
+    return arr[Math.floor(Math.random() * arr.length)];
 };
 
 function releafeTheBeaft() {
+    total = 0;
     beaft = {
-        "horns": getRandom(horns),
-        "ears": getRandom(ears),
-        "cheeks": getRandom(cheek),
-        "tail": getRandom(tail)
+        "horns": getRandom(data.horns),
+        "ears": getRandom(data.ears),
+        "cheeks": getRandom(data.cheek),
+        "tail": getRandom(data.tail)
     };
     let myString = "";
+    console.log(beaft["horns"][0]);
     for (const [trait, desc] of Object.entries(beaft)) {
-        myString = myString + `<p>${trait}: ${desc}</p>`;
+        total = total + desc[1];
+        myString = myString + `<span class="trait">${trait}:</span> <span class="desc">${desc[0]}</span>`;
     };
     document.getElementById("content").innerHTML = myString;
+    document.getElementById("total").innerHTML = `<span class="total">total cost: ${total}`
+    console.log(total);
     return;
 };
