2913e04cb23e7799a99086c35a135c9b9b882dce
sulphite <49396588+sulphite@users.noreply.github.com>
2020-09-17 14:10:42 +0200

    added weighted random function

--- random.js
+++ random.js
@@ -1,32 +1,49 @@
-const [a,b,c] = [1,2,3];
+const [a,b,c] = [
+    {cost: 1, weight: 60},
+    {cost: 2, weight: 30},
+    {cost: 3, weight: 10}
+];
+
 const data = {
     horns: [["small",a],["med",a],["large",a],["stubby",a],["slight curve",a],["ram",b],["angled",b],["curved",b],["lightning",c],["gravity",c]],
     ears: [["farm",a],["triangle",a],["long",a],["fan",b],["flop",b],["low",c]],
-    cheek: [["tuft",a],["bare",a],["fin",b],["pad",b],["gills",c],["fluff",b]],
+    cheeks: [["tuft",a],["bare",a],["fin",b],["pad",b],["gills",c],["fluff",b]],
     tail: [["lion",a],["tuft",a],["curl",b],["lizard",a],["horse",b],["brush",b],["bun",b],["fin",c],["paddle",c],["shark",c]]
 }
 
-let beaft = {};
-let total = 0;
-
-function getRandom(arr) {
+const getRandom = function(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 };
 
-function releafeTheBeaft() {
+const getWeightedRandom = function(arr) {
+    let total = 0;
+    for(let i = 0; i < arr.length; i++) {
+        total += arr[i][1].weight;
+    }
+    const threshold = Math.ceil(Math.random() * total);
+
     total = 0;
-    beaft = {
-        "horns": getRandom(data.horns),
-        "ears": getRandom(data.ears),
-        "cheeks": getRandom(data.cheek),
-        "tail": getRandom(data.tail)
-    };
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i][1].weight;
+        if(total >= threshold) {return arr[i]}
+    }
+    
+}
+
+function releafeTheBeaft(method) {
+    let total = 0;
+    let beaft = {};
     let myString = "";
+
+    for(let key in data) {
+        beaft[key] = method(data[key])
+    };
+
     for (const [trait, desc] of Object.entries(beaft)) {
-        total = total + desc[1];
+        total = total + desc[1].cost;
         myString = myString + `<span class="trait">${trait}:</span> <span class="desc">${desc[0]}</span>`;
     };
     document.getElementById("content").innerHTML = myString;
-    document.getElementById("total").innerHTML = `<span class="total">total cost: ${total}`
+    document.getElementById("total").innerHTML = `<span class="total">total cost: ${total}`;
     return;
 };
